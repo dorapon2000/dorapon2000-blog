42dcbfae367b4b6c1054a6927ee302f27d5a97eb
dorapon <dorapon2000@gmail.com>
2021-12-08 02:51:51 +0900

    自己紹介の関数を追加

--- git_pull_push/hello_world.py
+++ git_pull_push/hello_world.py
@@ -1,6 +1,11 @@
+def introduction(name):
+    print(f'I am {name}')
+
+
 def hello(place):
     print(f'hello {place}')
 
 
 if __name__ == '__main__':
+    introduction('dorapon')
     hello('world')
